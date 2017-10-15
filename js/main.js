1ebe35984c6a495097f3fa16762e260389636d1d
larsvers <larsverspohl@gmail.com>
2017-10-15 20:29:16 +0200

    Allow D3 lifecycle for pack drawing
    * Added unique id to nodes
    * Added key function to circle pack
    * Pack draw now with enter/update/exit
    
    * Extended radius scale API to pass in measure responsible for r

--- js/main.js
+++ js/main.js
@@ -6,13 +6,65 @@
 var vis = vis || {};
 
 vis.data;
+vis.radiusMeasure;
 vis.pack = {};
 vis.dims = { radius: 2, padding: 1 };
 vis.svg;
-vis.colourScale;
+vis.scale = { colour: undefined, r: undefined};
 vis.geo = {};
 vis.sim;
 
+
+/* Helpers */
+/* ------- */
+
+function setColourScale(measure) {
+
+	var domain = vis.pack.nodes.filter(function(el) {
+	  return !el.children;
+	}).reduce(function(a, b) {
+		if (a.indexOf(b.data[measure]) < 0) a.push(b.data[measure]);
+		return a;
+	}, [])
+
+	// var colScale = d3.scaleOrdinal(d3.schemeSet3).domain(domain);
+	var colScale = d3.scaleOrdinal().domain(domain).range(['#0086DC', '#FF7956', '#5A9700', '#9C237D', '#E6AA00', '#532F0C', '#aaa']);
+
+
+
+	vis.scale.colour = colScale;
+
+} // setColourScale()
+
+function setRadiusScale(measure) {
+
+	var extent = d3.extent(vis.pack.nodes, function(d) { return d.data[measure]; });
+
+	var rScale = d3.scaleSqrt().domain(extent).range([2, 5]);
+
+	vis.scale.r = rScale;
+
+} // setRadiusScale()
+
+function setRadii(unitSize) {
+
+	if (!arguments.length) { unitSize === false }
+
+	d3.selectAll('.node')
+		.transition()
+			.attr('r', function(d) { 
+
+				// save for easy pick up later at collision detection
+				d.rMap = d.children ? 0 : unitSize ? vis.dims.radius : vis.scale.r(d.data[vis.radiusMeasure])
+				return d.rMap;
+
+			});
+
+} // setRadii()
+
+
+/* General */
+/* ------- */
 
 function setupVisual() {
 
@@ -52,53 +104,42 @@ function getPack(data) {
 
 } // getPack()
 
-function layoutPackData(data) {
+function layoutPackData(data, measure) {
 
 	// layout data
   var root = d3.hierarchy(data)
-	    .sum(function(d) { return d.participating; })
+	    .sum(function(d) { return d[measure]; })
 	    .sort(function(a, b) { return b.value - a.value; });
 
 	var nodes = vis.pack.layout(root).descendants();
+
+	// Add unique id to each datapoint for circle pack key function
+	nodes.forEach(function(el) { el.id = el.children ? el.data.name : el.data.id; })
 
   vis.pack.root = root;
   vis.pack.nodes = nodes;
 
 } // layoutPackData()
 
-function getColourScale(measure) {
-
-	var domain = vis.pack.nodes.filter(function(el) {
-	  return !el.children;
-	}).reduce(function(a, b) {
-		if (a.indexOf(b.data[measure]) < 0) a.push(b.data[measure]);
-		return a;
-	}, [])
-
-	var colScale = d3.scaleOrdinal(d3.schemeSet3).domain(domain);
-
-	vis.colourScale = colScale;
-
-} // colourScale()
-
 function drawPack(data) {
-
-	getColourScale('school');
-
+	
 	var circles = vis.svg.append('g').attr('id', 'pack-g')
 			.selectAll('.node')
-			.data(data)
-		.enter().append('circle')
+			.data(data, function(d) { return d.id; });
+
+	circles.enter().append('circle')
+		.merge(circles)
 			.attr('class', function(d) { return d.parent ? d.children ? 'node' : 'node node-leaf' : 'node node-root'})
-			.style('fill', function(d) { return d.parent ? d.children ? '#e7e7e7' : vis.colourScale(d.data.school) : '#f3f3f3'; })
+			.style('fill', function(d) { return d.parent ? d.children ? '#e7e7e7' : vis.scale.colour(d.data.school) : '#f3f3f3'; })
 			.attr('transform', function(d) { 
 				d.xPack = d.x; // augment data with pack positions...
 				d.yPack = d.y;
 				return 'translate(' + d.x + ', ' + d.y + ')'; 
 			})
-			.attr('r', 0)
 		.transition().ease(d3.easeElastic).duration(1500).delay(function(d,i) { return i * 20; })
 			.attr('r', function(d) { return d.r; });
+
+	circles.exit().transition().attr('r', 0).remove()
 
 } // drawPack()
 
@@ -144,20 +185,13 @@ function drawMap(data) {
 } // drawMap()
 
 
-function setRadii() {
-
-	d3.selectAll('.node')
-		.transition()
-			.attr('r', function(d) { return d.children ? 0 : vis.dims.radius; });
-
-} // setRadii()
-
 function initMapSimulation(nodes) {
-// debugger
+
   vis.sim = d3.forceSimulation(nodes)
+  		.velocityDecay(0.2)
       .force('xPos', d3.forceX(function(d) { d.xGeo = vis.geo.projection([d.data.lng, d.data.lat])[0]; return d.xGeo }).strength(0.1)) // also augment map positions as xGeo and yGeo...
       .force('yPos', d3.forceY(function(d) { d.yGeo = vis.geo.projection([d.data.lng, d.data.lat])[1]; return d.yGeo; }).strength(0.1))
-      .force('collide', d3.forceCollide(vis.dims.radius + vis.dims.padding))
+      .force('collide', d3.forceCollide().radius(function(d) { return d.rMap + vis.dims.padding }))
     	.on('tick', tick);
 
   leaves = d3.selectAll('.node-leaf'); // save re-selection time in tick() TODO global
@@ -171,8 +205,8 @@ function initMapSimulation(nodes) {
 } // initMapSimulation()
 
 function mapSimulation() {
-// debugger
 	
+	// corce x to positions prior to map simulation === pack positions
 	leaves.data().forEach(function(el) {
     el.x = el.xPack;
     el.y = el.yPack;
@@ -198,7 +232,7 @@ function positionMap() {
 	var nodes = vis.pack.nodes.filter(function(el) { return !el.children; });
 
 	// shrink circles
-	setRadii();
+	setRadii(false);
 
 	// Move to map
 	!vis.sim ? initMapSimulation(nodes) : mapSimulation();
@@ -232,7 +266,11 @@ function ready(error, data, world) {
 
 	getPack(vis.data);
 
-	layoutPackData(vis.data);
+	vis.radiusMeasure = 'operating_weeks';
+
+	layoutPackData(vis.data, vis.radiusMeasure);
+
+	setColourScale('school');
 
 	drawPack(vis.pack.nodes);
 
@@ -245,11 +283,12 @@ function ready(error, data, world) {
 
 	setupMap(vis.geo.countries);
 
+	setRadiusScale(vis.radiusMeasure);
+
 	drawMap(vis.geo.countries);
 
 
 
-// d3.select('#one').on('mousedown', setRadii);
 
 d3.select('#one').on('mousedown', positionMap); 
 d3.select('#two').on('mousedown', positionPack); 
